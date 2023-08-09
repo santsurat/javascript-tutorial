fec2a248cb5f4d5d374a354f3c2c4661f33ccde2
@SantSurat <santsurat123@gmail.com>
2023-08-09 11:22:24 +0530

    update

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -175,11 +175,219 @@
 //   let fruit = "apple";
 // }
 
-function checkValue() {
-  console.log(score); // Output?
-  var score = 5;
-  {
-    var score = 10;
-  }
+// function checkValue() {
+//   console.log(score); // Output?
+//   var score = 5;
+//   {
+//     var score = 10;
+//   }
+// }
+// checkValue();
+
+// for (var i = 0; i < 5; ++i) {}
+// console.log(i);
+
+
+// function testScope() {
+//   var x = 10;
+// }
+// testScope();
+// console.log(x);
+
+// function foo() {
+//   if (true) {
+//     var y = 20;
+//   }
+// }
+// foo();
+// console.log(y);
+
+
+// var sum = 0;
+// for (var j = 1; j <= 3; j++) {
+//   console.log(j)
+//   sum += j;
+// }
+// console.log(j);
+
+
+// for (var a = 0; a < 3; a++) {
+//   var a = "Loop";
+// }
+// console.log(a);
+
+// for (var z = 1; z <= 3; z++) {
+//   var z = 2;
+// }
+// console.log(z);
+
+// for (var b = 1; b < 5; b++) {
+//   for (var b = 1; b < 3; b++) {}
+// }
+// console.log(b);
+// for (var d = 0; d < 1; d++) {
+//   var d = d + 1;
+// }
+// console.log(d);
+
+// Q2: Declare a variable using 'const' inside a function
+// Q9: Create a function and declare a variable using 'const' inside it
+// function myFunction() {
+//   var age = 30;
+//   if (true) {
+//     var age = 35;
+//     console.log(age);
+//   }
+//   console.log(age);
+// }
+
+// // Call the function
+// myFunction();
+
+// What do you observe?
+
+// function scopeAndRedeclarationChallenge() {
+//   // declare a variable with 'let'
+//   let letVariable = 10;
+
+//   // try to re-declare the same variable with 'var'
+//   // this will raise an error
+//   var varVariable = 20;
+
+//   // try to re-declare the same variable with 'const'
+//   // this will also raise an error
+//   const constVariable = 30;
+// }
+
+// if (typeof window !== "undefined") {
+//   window.onload = function() {
+//     scopeAndRedeclarationChallenge();
+//   };
+// }
+
+// function scopeChallenge1() {
+//   let myVariable = 10;
+//   if (true) {
+//     var myVariable = 20;
+//     console.log(myVariable);
+//   }
+//   console.log(myVariable);
+// }
+
+// scopeChallenge1();
+
+// function scopeChallenge2() {
+//   let myVariable = 10;
+//   if (true) {
+//     const myVariable = 20;
+//     console.log(myVariable);
+//   }
+//   console.log(myVariable);
+// }
+
+// scopeChallenge2();
+// function scopeChallenge3() {
+//   let myVariable = 10;
+//   {
+//     var myVariable = 20;
+//     console.log(myVariable);
+//   }
+//   console.log(myVariable);
+// }
+
+// scopeChallenge3();
+
+// function scopeChallenge9() {
+//   // myVariable = 10;
+//   {
+//     var myVariable = 20;
+//   }
+//   console.log(myVariable);
+// }
+
+// scopeChallenge9();
+// console.log(myVariable)
+
+// function scopeChallenge10() {
+//   let myVariable = 10;
+//   {
+//     const myVariable = 20;
+//   }
+//   console.log(myVariable);
+// }
+
+// scopeChallenge10();
+// console.log(myVariable)
+// var abc
+// function myVariable (){
+//     var a = 6
+// if(true){
+//    let abc = 5
+// console.log(abc)
+
+// }
+// console.log(a);
+// }
+
+// myVariable()
+
+// const person = { name: 'John', age: 25 };
+
+// person.city = 'New York';
+
+// console.log(person);
+
+// function challenge() {
+//    var c = 45;
+//    {
+//      let c = 55;
+//      console.log(c);
+//    }
+//    console.log(c);
+//  }
+//  challenge();
+ 
+// const hero = {
+//    firstName:'superman',
+//    age:10,
+
+// };
+
+// const Data= JSON.stringify(hero);
+// console.log(typeof Data);
+
+// const data = ["hello", "bolo","Hello"]
+// console.log(data);
+
+// console.log(data.join(" "));
+// console.log(typeof data);
+
+// const todayDate = new Date(2023,7,8 , 20 , 56) ;
+// const newDate = new Date();
+// console.log(newDate.toLocaleDateString());
+// console.log(newDate.toLocaleTimeString());
+
+// const myObj = {
+//     name : "ups",
+//     age :"29"
+// };
+// myObj.name= "surat";
+// Object.freeze(myObj);
+// Object.seal(myObj);
+
+// myObj.name= "ram";
+// delete myObj.name
+// myObj.class= "10th";
+
+// console.log(myObj);
+
+
+function TakeTwoNumber(number1, number2) {
+  // result = number1+number2
+  // console.log(result);
+  // console.log('hello')
+  return number1 + number2;
 }
-checkValue();
+
+const newResult = TakeTwoNumber(5, 8);
+console.log(newResult);
